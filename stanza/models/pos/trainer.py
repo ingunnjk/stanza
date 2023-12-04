b8382e5f5b5f94daf2d3d3a3af607ea409f16904
Houjun Liu <houjun@jemoka.com>
2023-12-04 09:32:23 -0800

    reduce rank to save memory

--- stanza/models/pos/trainer.py
+++ stanza/models/pos/trainer.py
@@ -19,10 +19,10 @@ logger = logging.getLogger('stanza')
 
 # our standard peft config
 PEFT_CONFIG = LoraConfig(inference_mode=False,
-                         r=16,
+                         r=8,
                          target_modules=["query", "value",
                                          "output.dense", "intermediate.dense"],
-                         lora_alpha=32,
+                         lora_alpha=16,
                          lora_dropout=0.1,
                          modules_to_save=[ "pooler" ],
                          bias="none")
